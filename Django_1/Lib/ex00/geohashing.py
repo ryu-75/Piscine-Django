c373223abc10195ac3e7258d372bfda30225da50
Nicolas LORION <nlorion@made-f0Br7s15.clusters.42paris.fr>
2024-09-23 17:22:09 +0200

    finish

--- Django_1/Lib/ex00/geohashing.py
+++ Django_1/Lib/ex00/geohashing.py
@@ -1,19 +1,22 @@
+#!/bin/python3
+import antigravity
 import sys
-# import antigravity
-
-# def geohashing(longitude, latitude, date):
-#     print(hash(date))
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        print("Usage: python3 geohashing.py [latitude] [longitude] [date]")
-        print("Example: python3 geohashin.py 37 -122 2005-05-26-10458.68")
-        exit
-    latitude = float(sys.argv[1])
-    longitude = float(sys.argv[2])
-    date = sys.argv[3]
-    
-    encode = date.encode('utf-8')
-    # geo = antigravity.geohash(latitude, longitude, encode)
-    print(latitude)
+    try:
+        if len(sys.argv) != 4:
+            print("Usage: python3 geohashing.py [latitude] [longitude] [date]")
+            print("Example: python3 geohashin.py 37 -122 2005-05-26-10458.68")
+            exit
+
+        latitude = float(sys.argv[2])
+        longitude = float(sys.argv[3])
+        date = sys.argv[4]
+        
+        encode = date.encode('utf-8')
+        geo = antigravity.geohash(latitude, longitude, encode)
+        print(geo)
+    except IndexError as e:
+        print(e)
+        
     
