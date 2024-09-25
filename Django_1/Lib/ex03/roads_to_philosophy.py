740b04a59666234619991902b9972d5dd067ee38
Sasha <sasha.lorion@gmail.com>
2024-09-25 15:01:34 +0200

    Lib/ex03 finishh

--- Django_1/Lib/ex03/roads_to_philosophy.py
+++ Django_1/Lib/ex03/roads_to_philosophy.py
@@ -1,83 +1,96 @@
 import sys, requests
 from bs4 import BeautifulSoup
 
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-
-def find_first_link(el):
-    content = {
-        'title': '',
-        'link': ''
-    }
+class roads_to_philosophy:
+    step: int = 1
     
-    for p in el.find_all('p'):
-        for a in p.find_all('a'):
-            href = a.get('href', '')
-            title = a.get('title', '')
-            if href.startswith('/wiki/') and not href.startswith(('/wiki/Help', '/wiki/Special', '#', '/wiki/Wikipedia', '/wiki/File')):
-                print(href) 
-                content['title'] = title
-                content['link'] = href
-                return content
-    print("Links are not found.")
-    return content
+    def __init__(self) -> None:
+        self.visitedLink = set()
+        self.content = []
+
+    def fetch_wikipage(self, link: str):
+        try:
+            res = requests.get(link)
+            res.raise_for_status()
+            return res
+        except requests.HTTPError as e:
+            if res.status_code == 404:
+                print("It's a dead end")
+                sys.exit(1)
+            print("An error occurred: {e}")
+            sys.exit(1)
+        
+
+    def scrap_wikipedia(self, link: str):
+        wiki_link = f"https://en.wikipedia.org/wiki/{link}"
+        # Fetch the webpage
+        res = self.fetch_wikipage(wiki_link)
+        # Parse the html content
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # Find the first link in introduction paragraph
+        el = soup.find('div', id='mw-content-text')
+        scrap = self.find_first_link(el)
+        # Add the starting page and the first link from the first paragraph
+        self.content.append(sys.argv[1])
+        self.content.append(scrap['title'])
+        self.step += 1
+        # Loop through the article until 'Philosophy' is found
+        while scrap['title'] != 'Philosophy':    
+            if not scrap['link']:
+                print("There is no link to visited.")
+                sys.exit(1)
+            # Visit the next link if it's lead to a wikipedia page
+            if '/wiki/' in scrap['link']:
+                wiki_link = f"https://en.wikipedia.org{scrap['link']}"
+                # Check if the link is already visited or not
+                if wiki_link not in self.visitedLink:
+                    self.visitedLink.add(wiki_link)
+                    # Repeat the second operation
+                    res = self.fetch_wikipage(wiki_link)
+                    soup = BeautifulSoup(res.text, 'html.parser')
+                    el = soup.find('div', id='mw-content-text')
+                    scrap = self.find_first_link(el)
+                    self.content.append(scrap['title'])
+                    self.step += 1
+                else:
+                    print("It leads to an infinite loop !")
+                    sys.exit(1)
+            else:
+                print("Link is invalid")
+                sys.exit(1)
+        self.display_visited_link(self.content)
+        print(f"{self.step} roads from {sys.argv[1]} to philosophy !")
+
+    
+    def display_visited_link(self, content):
+        for title in content:
+            print(title)
+
+    def find_first_link(self, el):
+        content = {
+            'title': '',
+            'link': ''
+        }
+        
+        for p in el.find_all('p'):
+            for a in p.find_all('a'):
+                href = a.get('href', '')
+                title = a.get('title', '')
+                if href.startswith('/wiki/') and not href.startswith(('/wiki/Help', '/wiki/Special', '#', '/wiki/Wikipedia', '/wiki/File', '/w/', "https")):
+                    content['title'] = title
+                    content['link'] = href
+                    return content
+        print("Links are not found.")
+        return content
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 2 and len(sys.argv[1]) != 0:
+        philosophy = roads_to_philosophy()
+        philosophy.scrap_wikipedia(sys.argv[1])
+    else:
         print(f"Usage: {sys.argv[0]} `cat`")
         sys.exit(1)
-    
-    wiki_link = f"https://en.wikipedia.org/wiki/{sys.argv[1]}"
-    r = requests.get(wiki_link)
-    nb_road = 0
-    visitedLink = set()
-    
-    # Fetch the data if the request success
-    if r.status_code != 200:
-        print("It's a dead end !")
-        sys.exit(1)
         
-    soup = BeautifulSoup(r.text, 'html.parser')
-    content = []
-     
-    # Find the first link in introduction paragraph
-    el = soup.find('div', class_='mw-parser-output')
-    scrap = find_first_link(el)
-    content.append(sys.argv[1])
     
-    # Check if a link exist
-    if scrap is not None:
-        content.append(scrap['title'])
-    else:
-        print("It's a dead end !")
-        sys.exit(1)
-        
-    # We loop until Philosophy is found
-    while 1:    
-        if not scrap['link']:
-            print("Link is not exist")
-            sys.exit(1)
-        
-        # We visit all links until philosophy is found
-        if '/wiki/' in scrap['link']:
-            l = f"https://en.wikipedia.org{scrap['link']}"
-            if l not in visitedLink:
-                visitedLink.add(l)
-                req = requests.get(l)
-                if (req.status_code != 200):
-                    print(f"Error: {req.status_code}")
-                    sys.exit(1)
-                soup = BeautifulSoup(req.text, 'html.parser')
-                el = soup.find('div', class_='mw-parser-output')
-                scrap = find_first_link(el)
-                nb_road += 1
-                content.append(scrap['title'])
-                if scrap['title'] == 'Philosophy':
-                    content.append(f"{nb_road} roads from {sys.argv[1]} to philosophy")
-                    break
-            else:
-                print("Link already visit")
-                sys.exit(1)
-        else:
-            print("Link is invalid")
-            sys.exit(1)
+
     
