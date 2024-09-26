73003650efb1803953f15e1c05646a9f1af75a46
Sasha <sasha.lorion@gmail.com>
2024-09-26 22:13:03 +0200

    wip

--- Django_1/Base_Django/ex00/models.py
+++ Django_1/Base_Django/ex00/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MarkdownContent(models.Model):
+    title = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Markdown content"
+        
+        def __str__(self):
+            return self.title
