90c093a7e357c1f754f435e774f82f02fdb31f8a
Sasha LORION <sasha.lorion@gmail.com>
2024-09-30 16:56:21 +0200

    started

--- Django_1/Base_Django/d04/settings.py
+++ Django_1/Base_Django/d04/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ex00',
-    'ex01',
+    'ex00.apps.Ex00Config',
+    'ex01.apps.Ex01Config',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Django_1/Base_Django/d04/urls.py
+++ Django_1/Base_Django/d04/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from ex00.views import ex00
-from ex01.views import html_render
+from django.urls.conf import include 
 
 urlpatterns = [
+    path('ex00/', include('ex00.urls')),
+    path('ex01/', include('ex01.urls')),
     path('admin/', admin.site.urls),
-    path('ex00/index', ex00),
-    path('ex01/<slug:slug>/', html_render, name="ex01"),
 ]
