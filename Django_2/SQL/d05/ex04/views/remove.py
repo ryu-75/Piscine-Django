a514025000329f43ef9892102e91175a0c156ca5
Sasha <sasha.lorion@gmail.com>
2024-10-07 21:56:28 +0200

    SQL/ex04 finish

--- Django_2/SQL/d05/ex04/views/remove.py
+++ Django_2/SQL/d05/ex04/views/remove.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.conf import settings
 import psycopg2, os
-import dotenv 
+import dotenv
 from django.views import View
 from ..forms import RemoveForm
 
@@ -10,54 +10,55 @@ dotenv.load_dotenv(os.path.join(settings.BASE_DIR, '.env'))
 
 name = os.getenv('DB_NAME')
 pwd = os.getenv('DB_PWD')
+user = os.getenv('DB_USER')
 port = os.getenv('DB_PORT')
-svc = os.getenv('DB_SERVICE')
 host = os.getenv('DB_HOST')
 
 TABLE_NAME = 'ex04_movies'
 
 class Remove(View):
     template = 'ex04/remove.html'
-    
+
     def get(self, request):
-        SELECT_DATA = '''
-            SELECT * FROM {table_name};
-        '''.format(table_name=TABLE_NAME)
         try:
-            conn = psycopg2.connect(database=name, password=pwd, port=port, host=host)
+            conn = psycopg2.connect(database=name, user=user, password=pwd, port=port, host=host)
+            # Establish the connection
             with conn.cursor() as cur:
-                cur.execute(SELECT_DATA)
-                rows = cur.fetchall()
-            context = {'form': RemoveForm(choices=((row[0], row[0]) for row in rows))}
+                cur.execute(f'''SELECT * FROM {TABLE_NAME};''')
+                movies = cur.fetchall()
+
+            # Prepare choices for the form
+            choices = [(movie[1], movie[1]) for movie in movies]
+            context = {'form': RemoveForm(choices)}
             return render(request, self.template, context)
         except Exception as e:
-            print(e)
+            print(f'Error fetching data: {e}')
             return HttpResponse('No data available')
 
     def post(self, request):
-        SELECT_DATA = '''
-            SELECT title FROM {table_name};
-        '''.format(table_name=TABLE_NAME)
         try:
-            conn = psycopg2.connect(database=name, password=pwd, port=port, host=host)
+            conn = psycopg2.connect(database=name, user=user, password=pwd, port=port, host=host)
             with conn.cursor() as cur:
-                cur.execute(SELECT_DATA,)
-                rows = cur.fetchall()
-            choices = ((row[0], row) for row in rows)
+                cur.execute(f'''SELECT title FROM {TABLE_NAME};''')
+                movies = cur.fetchall()
+
+            choices = [(movie[0], movie[0]) for movie in movies]
+
         except Exception as e:
-            print(f'{e}')
-            
-        data = RemoveForm(choices, request.POST)    
+            print(f'Error fetching data: {e}')
+
+        data = RemoveForm(choices, request.POST)
         if data.is_valid() == True:
-            DELETE = '''
-                DELETE FROM {table_name} WHERE title = %s
-            '''.format(table_name=TABLE_NAME)
+            print(f"{data.cleaned_data['title']}")
             try:
                 with conn.cursor() as cur:
-                    cur.execute(DELETE, [data.cleaned_data['title']])
+                    cur.execute(f'''
+                        DELETE FROM {TABLE_NAME} WHERE title = '{data.cleaned_data['title']}';
+                    ''')
                 conn.commit()
-                cur.close()
+                print('Data deleted successfully')
             except Exception as e:
                 print(f'No data to delete: {e}')
+            finally:
                 conn.close()
         return redirect(request.path)
