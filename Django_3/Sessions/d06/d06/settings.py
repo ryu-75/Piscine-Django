8ab5142df42a91e69e97ba2f7f47950b19368869
Sasha LORION <sasha.lorion@gmail.com>
2024-10-11 15:13:15 +0200

    day 06 started

--- Django_3/Sessions/d06/d06/settings.py
+++ Django_3/Sessions/d06/d06/settings.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
-"""
 
+https://www.pythontutorial.net/django-tutorial/django-sessions/
+"""
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,6 +52,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'd06.urls'
+
+# Age of session in second
+SESSION_COOKIE_AGE = 42
 
 TEMPLATES = [
     {
@@ -124,4 +128,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added 10 names in USERNAME
-# USERNAME=[]
+USERNAME=['Luffy', 'Jinbe', 'Zorro', 'Chopper', 'Sanji', 'Brook', 'Franky', 'Nami', 'Robin', 'Usopp']
--- Django_3/Sessions/d06/ex00/views/random.py
+++ Django_3/Sessions/d06/ex00/views/random.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpRequest
+from random import choice
+from django.conf import settings
 
 class   Random(View):
     template = 'ex00/base.html'
     def get(self, request):
-        return render(request, self.template)
+        names = request.session.get(settings.USERNAME)
+        return render(request, self.template, {'names':choice(names)})
