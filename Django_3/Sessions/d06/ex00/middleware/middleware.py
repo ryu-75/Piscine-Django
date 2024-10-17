f6b3098029077ee01c18eaaf1ead868a0cc92af6
Sasha LORION <sasha.lorion@gmail.com>
2024-10-17 16:05:28 +0200

    Middleware's work

--- Django_3/Sessions/d06/ex00/middleware/middleware.py
+++ Django_3/Sessions/d06/ex00/middleware/middleware.py
@@ -1,21 +1,18 @@
-from datetime import datetime
+import time
 from django.conf import settings
 import random
+from django.http import HttpRequest
+from  django.utils.deprecation import MiddlewareMixin 
 
-class   MiddlewareAnonymeSessions():
-    def __init__(self, get_response):
-        self.get_response = get_response
-        self.last_update = datetime.now().second
-        
-    def __call__(self, request):
-        current_time = datetime.now().second
-        new_time = current_time - self.last_update
-        print(self.last_update)
-        print(new_time)
-        if new_time >= 5:
-            name = settings.USERNAME
-            request.session['name'] = random.choice(name)
-            self.last_update = new_time
-        response = self.get_response(request)
-        
-        return response
+SESSION_TIME_EXPIRE = 42
+class   MiddlewareAnonymeSessions(MiddlewareMixin):
+    def process_request(self, request: HttpRequest):
+        if request.user.is_authenticated:
+            return
+        current_time = time.time()
+        init_time = request.session.setdefault('anonymous_timestamp', current_time)
+        if time.time() - init_time > SESSION_TIME_EXPIRE:
+            request.session.flush()
+        if 'anonymous' not in request.session:
+            request.session['anonymous'] = random.choice(settings.USERNAME)
+            request.session['anonymous_timestamp'] = current_time
